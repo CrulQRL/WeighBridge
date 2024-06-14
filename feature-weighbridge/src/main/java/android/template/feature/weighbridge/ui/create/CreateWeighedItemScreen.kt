2ca618aa2bcf9233e497348815a6610ad585802b
Faqrul <faqrul.anshurulloh@aruna.com>
2024-06-14 17:00:18 +0700

    Implement CTA button on Create and Edit screen

--- feature-weighbridge/src/main/java/android/template/feature/weighbridge/ui/create/CreateWeighedItemScreen.kt
+++ feature-weighbridge/src/main/java/android/template/feature/weighbridge/ui/create/CreateWeighedItemScreen.kt
@@ -1,13 +1,17 @@
 package android.template.feature.weighbridge.ui.create
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,15 +50,23 @@ fun CreateWeighedItemScreen(navController: NavController) {
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
+                            contentDescription = "Back button"
                         )
                     }
                 }
             )
         }
     ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth()
+        ) {
+
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -64,6 +77,9 @@ fun CreateWeighedItemScreen(navController: NavController) {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -74,6 +90,9 @@ fun CreateWeighedItemScreen(navController: NavController) {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -84,6 +103,9 @@ fun CreateWeighedItemScreen(navController: NavController) {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -104,12 +126,31 @@ fun CreateWeighedItemScreen(navController: NavController) {
                 )
 //              backgroundColor = MaterialTheme.colors.onSurface
             ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Text(text = "Net Weight", fontSize = 22.sp)
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = "Net Weight", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth())
+                {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(text = "Net Weight", fontSize = 22.sp)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = "Net Weight", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                    }
+
+                    Button(
+                        modifier = Modifier.padding(16.dp),
+                        shape = RoundedCornerShape(8.dp),
+                        onClick = {  }
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(vertical = 4.dp),
+                            text = "Submit", fontSize = 24.sp
+                        )
+                    }
+
                 }
             }
 
--- feature-weighbridge/src/main/java/android/template/feature/weighbridge/ui/edit/EditWeighedItemScreen.kt
+++ feature-weighbridge/src/main/java/android/template/feature/weighbridge/ui/edit/EditWeighedItemScreen.kt
@@ -1,13 +1,17 @@
 package android.template.feature.weighbridge.ui.edit
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,15 +48,23 @@ fun EditWeighedItemScreen() {
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
+                            contentDescription = "Back button"
                         )
                     }
                 }
             )
         }
     ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth()
+        ) {
+
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -62,6 +75,9 @@ fun EditWeighedItemScreen() {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -72,6 +88,9 @@ fun EditWeighedItemScreen() {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -82,6 +101,9 @@ fun EditWeighedItemScreen() {
             )
 
             OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp),
                 value = "",
                 onValueChange = { newText ->
 
@@ -102,12 +124,31 @@ fun EditWeighedItemScreen() {
                 )
 //              backgroundColor = MaterialTheme.colors.onSurface
             ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Text(text = "Net Weight", fontSize = 22.sp)
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = "Net Weight", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth())
+                {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(text = "Net Weight", fontSize = 22.sp)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = "Net Weight", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                    }
+
+                    Button(
+                        modifier = Modifier.padding(16.dp),
+                        shape = RoundedCornerShape(8.dp),
+                        onClick = {  }
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(vertical = 4.dp),
+                            text = "Save", fontSize = 24.sp
+                        )
+                    }
+
                 }
             }
 
