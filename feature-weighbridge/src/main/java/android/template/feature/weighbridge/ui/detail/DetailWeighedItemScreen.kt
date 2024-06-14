fe31fbb2a80e3bc196370c2f4d4042a785eec70f
Faqrul <faqrul.anshurulloh@aruna.com>
2024-06-14 16:19:13 +0700

    Implement Edit Item entry point button for navigating to Edit Screen

--- feature-weighbridge/src/main/java/android/template/feature/weighbridge/ui/detail/DetailWeighedItemScreen.kt
+++ feature-weighbridge/src/main/java/android/template/feature/weighbridge/ui/detail/DetailWeighedItemScreen.kt
@@ -1,19 +1,34 @@
 package android.template.feature.weighbridge.ui.detail
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -23,14 +38,58 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
+@ExperimentalMaterial3Api
 @Composable
 fun DetailWeighedItemScreen() {
 
     Scaffold(
-        topBar = {}
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text("Detail Item")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        }
     ) { paddingValues ->
 
         Column(modifier = Modifier.padding(paddingValues)) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Box(modifier = Modifier
+                    .wrapContentSize()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                    .border(2.dp, MaterialTheme.colorScheme.primary,RoundedCornerShape(8.dp))
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = "Edit Item", fontSize = 16.sp)
+                        Spacer(
+                            modifier = Modifier
+                                .width(8.dp)
+                        )
+                        Icon(
+                            imageVector = Icons.Filled.Create,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            }
 
             Text(
                 modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
@@ -156,6 +215,7 @@ fun DetailWeighedItemScreen() {
     }
 }
 
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 private fun Preview() {
